f2ee99105c7d14785969a17fbe8a7b424eedb5b9
DaySlacker <111376118+AshlynBarrera@users.noreply.github.com>
2023-01-26 09:26:43 -0600

    Initial Commit

---
+++ swiftFunctionsExercise.playground/Contents.swift
@@ -0,0 +1,41 @@
+
+
+
+//1
+func phineasFerb(){
+    print("Hey Ferb, I know what we're gonna do today!" )
+}
+phineasFerb()
+
+//2
+func agentP( perry: String){
+    print(perry + "Doo-Bee-Do-Bah")
+}
+
+//3
+func doof(name:String , animal: String ){
+    print("Curse you" + name + "The" + animal + "!")
+    }
+doof(name: "perry", animal: "Platypus")
+//4
+
+func isabella() -> String{
+   return "What'cha Doin?"
+}
+
+isabella()
+//Bonus
+//1a
+let characterOne = ["Phineas Flynn", "Ferb Fletcher", "Perry the Platypus"]
+//1b
+let characterTwo = ["Dr. Heinz Doofenshmirtz", "Candace Flynn", "Baljeet"]
+
+//1c
+func randomChar(char: [String]){
+    print(char.randomElement())
+}
+
+//1d
+randomChar(char: characterOne)
+randomChar(char: characterTwo)
+
