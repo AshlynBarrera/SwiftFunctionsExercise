69afa4d9809124511a52c08e45aa785a2b0660c3
DaySlacker <111376118+AshlynBarrera@users.noreply.github.com>
2023-01-27 08:35:25 -0600

    made modifications to the code.

--- swiftFunctionsExercise.playground/Contents.swift
+++ swiftFunctionsExercise.playground/Contents.swift
@@ -1,32 +1,40 @@
 
 
 
+
 //1
 func phineasFerb(){
     print("Hey Ferb, I know what we're gonna do today!" )
 }
 phineasFerb()
 
+
 //2
 func agentP( perry: String){
-    print(perry + "Doo-Bee-Do-Bah")
+    print(perry + perry + "Do-Bah")
 }
+agentP( perry: "Doo-Bee-")
 
 //3
 func doof(name:String , animal: String ){
-    print("Curse you" + name + "The" + animal + "!")
+    print("Curse you," + name + "The" + animal + "!")
     }
 doof(name: "perry", animal: "Platypus")
+
 //4
 
 func isabella() -> String{
    return "What'cha Doin?"
 }
+print(isabella())
 
-isabella()
+
+
 //Bonus
+
 //1a
 let characterOne = ["Phineas Flynn", "Ferb Fletcher", "Perry the Platypus"]
+
 //1b
 let characterTwo = ["Dr. Heinz Doofenshmirtz", "Candace Flynn", "Baljeet"]
 
@@ -38,4 +46,5 @@ func randomChar(char: [String]){
 //1d
 randomChar(char: characterOne)
 randomChar(char: characterTwo)
+
 
